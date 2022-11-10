5a4fc313d50caf69b8689fddcf2e4aaa2013ebbb
JasonBoyett <jason.boyett@gmail.com>
2022-11-09 21:02:18 -0600

    first commit

---
+++ advanced-java-final/src/main/java/jaboye2448/App.java
@@ -0,0 +1,15 @@
+package jaboye2448;
+
+import com.github.javafaker.Faker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        
+        TestUser test = new TestUser();
+        System.out.println(new TestUser().toString());
+    }
+}
